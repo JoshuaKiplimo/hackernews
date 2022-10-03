bb97e82f2dcd8864054111ecdd97e5b3241f4a39
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 22:14:44 -0400

    Updates

---
+++ precache-manifest.c111eb4d37c60dbd163d7bdc3830897c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715818cbd58cc3736f4b1b0bc34f6d1d",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "7f2027d832f9f15241fc",
+    "url": "/hackernews/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/hackernews/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackernews/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2027d832f9f15241fc",
+    "url": "/hackernews/static/js/main.602c5af8.chunk.js"
+  },
+  {
+    "revision": "914cab7ae5a96bbcbf0a",
+    "url": "/hackernews/static/js/runtime-main.18e8b03f.js"
+  }
+]);
